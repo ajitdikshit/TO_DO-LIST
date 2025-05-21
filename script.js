45d8ac86f62b86587b5b8cbb79fbfad7343078f3
ajitdikshit <dikshitajit32@gmail.com>
2025-05-21 22:29:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const listcontainer=document.getElementById("list-container");
+const inputbox=document.getElementById("input-box");
+
+
+function addTask(){
+    if(inputbox.value===''){
+        alert("PLEASE ENTER SOME TEXT");
+    }else{
+        let li=document.createElement("li");
+        li.innerHTML=inputbox.value;
+        listcontainer.appendChild(li);
+
+        let span=document.createElement("span");
+        span.innerHTML='\u00d7';
+        li.appendChild(span);
+    }
+    inputbox.value="";
+    savetask();
+}
+
+
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName ==="LI"){
+        e.target.classList.toggle("checked");
+        savetask();
+    }
+    else if(e.target.tagName ==="SPAN"){
+        e.target.parentElement.remove();
+        savetask();
+    }
+
+
+})
+
+
+function savetask(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+function showtask(){
+    listcontainer.innerHTML=localStorage.getItem("data");
+}
+
+showtask();
